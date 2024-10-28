619d07147a67a1d77cb4197fd88870505dc7406b
OmarIdrisT <146827333+OmarIdrisT@users.noreply.github.com>
2024-10-28 16:04:57 +0100

    Acabat

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-let valor = 0
+let player = 0
+let hand = 0
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     basic.showLeds(`
@@ -8,10 +9,11 @@ input.onButtonPressed(Button.A, function () {
         . # # # .
         . . # . .
         `)
+    player = 1
 })
 input.onGesture(Gesture.Shake, function () {
-    valor = randint(1, 3)
-    if (valor == 1) {
+    hand = randint(1, 3)
+    if (hand == 1) {
         basic.clearScreen()
         basic.showLeds(`
             . . # . .
@@ -21,7 +23,7 @@ input.onGesture(Gesture.Shake, function () {
             . . # . .
             `)
     }
-    if (valor == 2) {
+    if (hand == 2) {
         basic.clearScreen()
         basic.showLeds(`
             . # # # .
@@ -31,14 +33,24 @@ input.onGesture(Gesture.Shake, function () {
             . # # # .
             `)
     }
-    if (valor == 3) {
+    if (hand == 3) {
         basic.clearScreen()
         basic.showIcon(IconNames.Scissors)
+    }
+    if (hand == player) {
+        basic.showIcon(IconNames.Asleep)
+    } else {
+        if (hand == 3 && player == 1 || hand < player && player - hand == 1) {
+            basic.showIcon(IconNames.Happy)
+        } else {
+            basic.showIcon(IconNames.Sad)
+        }
     }
 })
 input.onButtonPressed(Button.AB, function () {
     basic.clearScreen()
     basic.showIcon(IconNames.Scissors)
+    player = 3
 })
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
@@ -49,4 +61,5 @@ input.onButtonPressed(Button.B, function () {
         . # # # .
         . # # # .
         `)
+    player = 2
 })
